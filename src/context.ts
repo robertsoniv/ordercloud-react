107a450e7d9064eafd0569a327a6b2a52c6b8904
Erin <erincdustin@gmail.com>
2024-03-22 13:15:14 -0700

    add token to context

--- src/context.ts
+++ src/context.ts
@@ -14,6 +14,7 @@ const INITIAL_ORDERCLOUD_CONTEXT: IOrderCloudContext = {
   scope: [],
   customScope: [],
   allowAnonymous: false,
+  token: undefined
 };
 
 export const OrderCloudContext = createContext<IOrderCloudContext>(
