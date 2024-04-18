a0e6e07cced04b4e68425e9920f6434b29b040a4
Erin <erincdustin@gmail.com>
2024-04-18 09:50:33 -0700

    add xp to useColumn properties if schema exists

--- src/hooks/useColumns.ts
+++ src/hooks/useColumns.ts
@@ -2,20 +2,34 @@ import { useMemo } from 'react'
 import useApiSpec from './useApiSpec';
 import Case from 'case';
 import { OpenAPIV3 } from 'openapi-types';
+import { useOrderCloudContext } from '.';
 
 const useColumns = (resourceId: string) => {
+  const { xpSchemas } = useOrderCloudContext()
   const { operationsById } = useApiSpec()
   const operationId = `${resourceId.charAt(0).toUpperCase() + Case.camel(resourceId.slice(1))}.List`
 
   const operation = useMemo(() => operationsById[operationId], [operationId, operationsById])
+
+  const xpProperties = useMemo(() => {
+    if(xpSchemas?.properties && xpSchemas.properties[resourceId]){
+      return xpSchemas.properties[resourceId]
+    } else {
+      return null
+    }
+  },[resourceId, xpSchemas])
 
   const properties = useMemo(() => {
     const response = operation?.responses['200'] as OpenAPIV3.ResponseObject
     const schema = response?.content?.['application/json']?.schema as OpenAPIV3.SchemaObject
     const schemaItemArray = schema?.properties?.Items as OpenAPIV3.ArraySchemaObject
     const schemaObj = schemaItemArray?.items as OpenAPIV3.SchemaObject
+    
+    if(xpProperties && schemaObj?.properties) 
+      schemaObj.properties.xp = xpProperties
+    
     return schemaObj?.properties
-  }, [operation])
+  }, [operation?.responses, xpProperties])
 
   const columnHeaders = useMemo(() => {
     if(!properties) return
